9ca123425f05682a63d94a1bf99cf94d92223b1b
Alex Hannani <alexhannani24@gmail.com>
2022-12-07 02:58:21 -0500

    added listener for click event on save button

--- assets/script.js
+++ assets/script.js
@@ -4,5 +4,21 @@ $(jQuery).ready(function () {
   var today = dayjs().format('dddd MMM D, YYYY')
   $('#currentDay').text("Today is " + today)
 
-  
+  // Listener for click event on save button
+  $(".saveBtn").click(function () {
+    var parent = $(this).parents().eq(0)
+    var clickedHour = parent.attr('id')
+
+    clickedHour = clickedHour.slice(5)
+    var slicedHour = clickedHour.slice(0, -2)
+
+    var textDescription = parent.find(".description").val();
+
+    function saveText() {
+      var saveText = localStorage.setItem("textDescription", textDescription)
+      saveText()
+    }
+
+    
+  }
 }
